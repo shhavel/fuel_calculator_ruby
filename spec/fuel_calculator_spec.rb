a92c2e1ec0fcbf9053386690d07801ae646aa694
Oleksandr Avoiants <shhavel@gmail.com>
2024-04-26 08:11:08 +0300

    More specs. Remove extra validation.

--- spec/fuel_calculator_spec.rb
+++ spec/fuel_calculator_spec.rb
@@ -27,4 +27,27 @@ RSpec.describe FuelCalculator do
       expect(FuelCalculator.call(mass, path)).to eq(212161)
     end
   end
+
+  context "Invalid arguments" do
+    context "invalid mass" do
+      it "raises exception" do
+        expect { FuelCalculator.call("str", [[:launch, "earth"], [:land, "mars"], [:launch, "mars"], [:land, "earth"]]) }.to raise_error FuelCalculator::ValidationError, "Invalid mass"
+      end
+    end
+
+    context "invalid path" do
+      it "raises exception" do
+        expect { FuelCalculator.call(75432, [[:launch, "earth"], [:launch, "moon"], [:land, "mars"]]) }.to raise_error FuelCalculator::ValidationError, "Invalid path"
+        expect { FuelCalculator.call(75432, [[:launch, "earth"], [:land, "moon"], [:launch, "mars"], [:land, "earth"]]) }.to raise_error FuelCalculator::ValidationError, "Invalid path"
+        expect { FuelCalculator.call(75432, [[:land, "earth"], [:launch, "earth"], [:land, "earth"]]) }.to raise_error FuelCalculator::ValidationError, "Invalid path"
+        expect { FuelCalculator.call(75432, [[:land, "earth"], [:land, "moon"], [:launch, "moon"]]) }.to raise_error FuelCalculator::ValidationError, "Invalid path"
+      end
+    end
+
+    context "unknown planet" do
+      it "raises exception" do
+        expect { FuelCalculator.call(28801, [[:launch, "earth"], [:land, "saturn"], [:launch, "saturn"], [:land, "earth"]]) }.to raise_error FuelCalculator::ValidationError, "Unknown planet"
+      end
+    end
+  end
 end
